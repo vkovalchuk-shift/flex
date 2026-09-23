ee231e3379c414819fecad6d98c4f49175fffae8
vkovalchuk-shift <v.kovalchuk@cleack.com>
2026-09-23 16:22:45 +0300

    create header, footer, hero section and insights section

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 import "../styles/main.scss";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header');
+    const menuTriggers = document.querySelectorAll('#btn-menu, .header-link');
+
+    menuTriggers.forEach(menuTrigger => {
+        menuTrigger.onclick = function() {
+            header.classList.toggle('menu-active');
+        }
+    });
+
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('is-sticky', window.scrollY >= header.offsetHeight);
+    });
+});
