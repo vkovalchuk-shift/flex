b559d18c94b8e896f65334faa593b8a29be9deff
vkovalchuk-shift <v.kovalchuk@cleack.com>
2026-09-25 18:00:07 +0300

    working on about page; refactoring

--- src/js/index.js
+++ src/js/index.js
@@ -13,4 +13,26 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', () => {
         header.classList.toggle('is-sticky', window.scrollY >= header.offsetHeight);
     });
+
+    function checkHeaderTheme() {
+        const rect = header.getBoundingClientRect();
+
+        const x = rect.left + rect.width / 2;
+        const y = rect.bottom - 2;
+
+        header.style.pointerEvents = 'none';
+
+        const element = document.elementFromPoint(x, y);
+
+        header.style.pointerEvents = '';
+
+        const darkElement = element?.closest('.dark-theme');
+
+        header.classList.toggle('dark-theme', !!darkElement);
+    }
+
+    window.addEventListener('scroll', checkHeaderTheme, { passive: true });
+    window.addEventListener('resize', checkHeaderTheme);
+
+    checkHeaderTheme();
 });
